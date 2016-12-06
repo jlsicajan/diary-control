ee5087b7e72647b8ff2680d4b5ffb0b15b3ceb13
Jose Luis Sicajan Coy <sicajancoy@gmail.com>
2016-12-05 22:17:43 -0600

    Auth and migration table work

---
+++ database/migrations/2016_12_06_025520_create_table_work.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableWork extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('description');
+            $table->dateTime('work_date');
+            $table->text('bussiness');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work');
+    }
+}
